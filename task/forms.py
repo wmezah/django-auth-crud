4d1bc20b2614d9c586a6ed0f9aedac4f779a03f6
Wilder Meza <wmezah@hotmail.com>
2025-04-10 23:43:18 -0500

    First commit

---
+++ task/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input form-check-inline'}),
+        }
+      
